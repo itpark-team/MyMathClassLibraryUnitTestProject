5b093b3fe7576b915050892a73f22652a6554c4c
itpark-team <46758840+itpark-team@users.noreply.github.com>
2022-02-05 12:11:02 +0300

    Добавьте файлы проекта.

---
+++ MyMathClassLibraryUnitTestProject/PlainArithmetic_UnitTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MyMathClassLibrary;
+
+namespace MyMathClassLibraryUnitTestProject
+{
+    [TestClass]
+    public class PlainArithmetic_UnitTest
+    {
+        [TestMethod]
+        public void Sum_input_3_5_output_8()
+        {
+            PlainArithmetic plainArithmetic = new PlainArithmetic();
+
+            double expected = 8;
+            
+            double actual = plainArithmetic.Sum(3, 5);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Mul_input_2_2_output_4()
+        {
+            PlainArithmetic plainArithmetic = new PlainArithmetic();
+
+            double expected = 4;
+
+            double actual = plainArithmetic.Mul(2, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MyMathClassLibraryUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyMathClassLibraryUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyMathClassLibraryUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("50561069-4d67-4930-a300-5e759f378e12")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
